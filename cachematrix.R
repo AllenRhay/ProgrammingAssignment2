178846ac0983fe73493c64ed29bbfc1eab57ad8d
AllenRhay <77728485+AllenRhay@users.noreply.github.com>
2021-01-20 19:16:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+setwd('C:/Users/rubind1/Documents/Coursera-R')
+##
+## Set the input x as a matrix
+## Then set the solved value "q" as a null
+## Then change every reference to "mean" to "solve"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  q <- NULL
+  set <- function(y) {
+    x <<- y
+    q <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) q <<- solve
+  getsolve <- function() q
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+##
+## Same here, change "mean" to "solve" and "m" to "s"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  q <- x$getsolve()
+  if(!is.null(q)) {
+    message("getting inversed matrix")
+    return(q)
+  }
+  data <- x$get()
+  q <- solve(data, ...)
+  x$setsolve(q)
+  q
 }
